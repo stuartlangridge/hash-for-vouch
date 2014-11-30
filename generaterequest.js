67f5f1ea1dd1884be7c77a4eb64e20855031c05e
Stuart Langridge <sil@kryogenix.org>
2014-11-30 15:03:00 +0000

    Design. Write documentation. Set number of 0s required to 5, which takes between a few seconds and a minute or so. Add database.

--- generaterequest.js
+++ generaterequest.js
@@ -1,5 +1,6 @@
 var crypto = require("crypto"),
-    qs = require("querystring");
+    qs = require("querystring"),
+    request = require("request");
 
 var counter = 0,
     t = Math.round(new Date().getTime() / 1000),
@@ -9,8 +10,12 @@ while (true) {
     sha256.update(url + "-" + t + "-" + counter);
     dig = sha256.digest('hex');
     //console.log(counter, dig);
-    if (dig.match(/^000000/)) {
-        console.log(qs.stringify({target: url, nonce: counter, time: t}));
+    if (dig.match(/^00000/)) {
+        var fields = {source: url, nonce: counter, time: t};
+        console.log("Requesting with parameters", qs.stringify(fields));
+        request.post({url: 'http://localhost:3000/endpoint', form: fields}, function(err, httpResponse, body) {
+            console.log("Response", body);
+        });
         break;
     }
     counter += 1;
--- index.js
+++ index.js
@@ -1,21 +1,95 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    crypto = require('crypto');
+    crypto = require('crypto'),
+    pg = require('pg'),
+    exphbs  = require('express-handlebars');;
+
+// Create database contents table on startup
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('create table if not exists vouches ' +
+    '(id serial primary key, source varchar, ' +
+    'created timestamp DEFAULT now() NOT NULL)', function(err, result) {
+        if (err) {
+            done(); throw new Error(err);
+        } else {
+            // Create function to delete rows after time N
+            // thank you http://www.the-art-of-web.com/sql/trigger-delete-old/
+            var fn = 'CREATE OR REPLACE FUNCTION delete_old_rows() RETURNS trigger ' +
+            'LANGUAGE plpgsql AS $$ BEGIN ' +
+            "DELETE FROM vouches WHERE created < NOW() - INTERVAL '3 minutes';" +
+            'RETURN NEW; END; $$;';
+            client.query(fn, function(err, result) {
+                if (err) {
+                    done(); throw new Error(err);
+                } else {
+                    // and trigger that function to be called whenever we add new rows
+                    var dtr = 'DROP TRIGGER IF EXISTS old_rows_gc ON vouches;';
+                    client.query(dtr, function(err, result) {
+                        if (err) {
+                            done(); throw new Error(err);
+                        } else {
+                            var tr = 'CREATE TRIGGER old_rows_gc AFTER INSERT ON vouches ' +
+                            'EXECUTE PROCEDURE delete_old_rows();';
+                            client.query(tr, function(err, result) {
+                                if (err) {
+                                    done(); throw new Error(err);
+                                } else {
+                                    console.log("Database set up OK", result);
+                                    done(); 
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 app.get('/', function (req, res) {
-  res.send('This is hash for vouch. POST to /endpoint. Docs go here.');
+    res.render('home');
+});
+
+app.get('/details', function (req, res) {
+    res.render('details');
+});
+
+app.get('/vouch/:id', function (req, res) {
+    if (isNaN(parseInt(req.params.id, 10))) {
+        return res.status(400).render("error", {error: "That isn't a valid vouch ID."});
+    }
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            console.log("Error connecting to db for vouch", req.params.id, err);
+            return res.status(500).render("error", {error: "There was a problem connecting to the database. Sorry."});
+        }
+        client.query('select source, created from vouches where id = $1::int', [req.params.id], function(err, result) {
+            done(); // release client back to the pool
+            if (err) {
+                console.log("Error selecting for vouch", req.params.id, err);
+                return res.status(500).render("error", {error: "There was a problem retrieving that vouch; sorry. Try again."});
+            }
+            if (result.rows.length < 1) {
+                return res.status(404).render("error", {error: "That vouch doesn't seem to exist."});
+            }
+            res.render("vouch", {source: result.rows[0].source});
+        });
+    });
 });
 
 app.post('/endpoint', function(req, res) {
-    if (!req.body || !req.body.target || !req.body.nonce || !req.body.time) {
+    if (!req.body || !req.body.source || !req.body.nonce || !req.body.time) {
         return res.status(400).json({error: "Incomplete request"});
     }
     var time = parseInt(req.body.time, 10);
@@ -26,12 +100,30 @@ app.post('/endpoint', function(req, res) {
         return res.status(400).json({error: "Request too old"});
     }
     var sha256 = crypto.createHash('sha256');
-    sha256.update(req.body.target + "-" + req.body.time + "-" + req.body.nonce);
-    if (!sha256.digest('hex').match(/^000000/)) {
+    sha256.update(req.body.source + "-" + req.body.time + "-" + req.body.nonce);
+    var dig = sha256.digest('hex');
+    if (!dig.match(/^00000/)) {
         return res.status(400).json({error: "That's a bad hash, Harry"});
     }
 
-    // OK now save it in a database, and return the URL to the page which displays it.
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            return res.status(500).json({error: "Couldn't save these details to vouch for you; sorry. Try again."});
+        }
+        client.query('insert into vouches (source) values ($1::varchar) returning id', [req.body.source], function(err, result) {
+            done(); // release client back to the pool
+            
+            if (err) {
+                return res.status(500).json({error: "Couldn't save when trying to vouch for you; sorry. Try again."});
+            } else {
+                var vouchurl = req.originalUrl.replace(/endpoint$/, "vouch/" + result.rows[0].id);
+                res.json({url: req.protocol + '://' + req.get('host') + vouchurl});
+            }
+        });
+    });
+    return;
+
+
     res.json({});
 });
     
